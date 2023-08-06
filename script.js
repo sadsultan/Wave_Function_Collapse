502012c1f15f5ad83133a8b6eef6d0daa366e6aa
Saad <66124515+sadsultan@users.noreply.github.com>
2023-08-06 15:35:47 +0500

    Reworked newGrid function

--- script.js
+++ script.js
@@ -12,19 +12,17 @@ class Tile {
 // holds tiles that are in the order: blank, up, right, down, left.
 const TILES = [new Tile(0,0,0,0, 'blank'), new Tile(1,1,0,1,'up'), new Tile(1,1,1,0,'right'), new Tile(0,1,1,1,'down'), new Tile(1,0,1,1,'left')]
 
-// create a grid object that has 2 nexter arrays : grid and possibilities
-// grid will hold the index of what tile from TILES is to be placed there and 
-// possibilities will hold the number of possible tiles that can be placed there
+// size of grid, ONLY FOR TESTING PURPOSES
+const SIZE = 3;
+
+// create a grid using nested arrays that has all the tiles in each spot 
 function newGrid(size){
-    let grid=[]
-    let possibilities=[]
-    for(let i = 0 ; i < size ; i++) {
-        grid.push([])
-        possibilities.push([])
+    let grid = []
+    for (let i = 0; i < size; i++) {
+        grid.push([]);
         for (let j = 0; j < size; j++) {
-            grid[i].push(-1)
-            possibilities[i].push(TILES.length)
+            grid[i].push(TILES.slice());
         }
     }
-    return {grid : grid, possibilities : possibilities};
+    return grid
 }
