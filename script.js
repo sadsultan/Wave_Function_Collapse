025754e054ed7ff4f63d03b6ece6445cc2cc816a
Saad <66124515+sadsultan@users.noreply.github.com>
2023-08-06 16:47:40 +0500

    Added Tilekey and removed Tile class

--- script.js
+++ script.js
@@ -1,26 +1,49 @@
-// Tiles are assigned the value clockwise from the top
-class Tile {
-    constructor(up, right, down, left, name) {
-        this.name = name; // allows for checking and also displaying the tile later
-        this.up = up;
-        this.down = down;
-        this.left = left;
-        this.right = right;
-    }
+// Key for what tiles can have what neighbours and in what directions.
+const TILEKEY = {
+    blank: {
+        'up': ['up', 'blank'],
+        'down': ['blank', 'down'],
+        'left': ['left', 'blank'],
+        'right': ['blank', 'right']
+    },
+    up: {
+        'up': ['down', 'left', 'right'],
+        'down': ['down', 'blank'],
+        'left': ['up', 'down', 'right'],
+        'right': ['up', 'down', 'left']
+    },
+    down: {
+        'up': ['up', 'blank'],
+        'down': ['up', 'left', 'right'],
+        'left': ['down' ,'up', 'right'],
+        'right': ['down' ,'up', 'left']
+    },
+    left: {
+        'up': ['down', 'left', 'right'],
+        'down': ['up', 'right', 'left'],
+        'left': ['right', 'down', 'up'],
+        'right': ['blank', 'right']
+    },
+    right: {
+        'up': ['right', 'down', 'left'],
+        'down': ['up', 'left', 'right'],
+        'left': ['left', 'blank'],
+        'right': ['up', 'down', 'left']
+    }        
 }
 
-// holds tiles that are in the order: blank, up, right, down, left.
-const TILES = [new Tile(0,0,0,0, 'blank'), new Tile(1,1,0,1,'up'), new Tile(1,1,1,0,'right'), new Tile(0,1,1,1,'down'), new Tile(1,0,1,1,'left')]
+// holds tile names
+const TILES = ['up','down','left','right','blank']
 
 // size of grid, ONLY FOR TESTING PURPOSES
 const SIZE = 3;
 
 // create a grid using nested arrays that has all the tiles in each spot 
-function newGrid(size){
+function newGrid(){
     let grid = []
-    for (let i = 0; i < size; i++) {
+    for (let i = 0; i < SIZE; i++) {
         grid.push([]);
-        for (let j = 0; j < size; j++) {
+        for (let j = 0; j < SIZE; j++) {
             grid[i].push(TILES.slice());
         }
     }
